754fca7d45d98945ca1509f99f67c760c2f69f8a
taroosg <root@taroosg.dev>
2020-11-06 15:59:07 +0000

    update config environment variables

---
+++ src/db.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE, } from './app.config';
+
+export const dbConfig: TypeOrmModuleOptions = {
+  type: "postgres",
+  host: POSTGRES_HOST,
+  port: Number(POSTGRES_PORT),
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
+  entities: [
+    "dist/entities/**/*.entity.js"
+  ],
+  migrations: [
+    "dist/migrations/**/*.js"
+  ],
+  synchronize: false,
+};
